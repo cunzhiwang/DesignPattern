495b7cdb1d25e31141e875d79242575a9f054a9a
cunzhiwang <2539029477@qq.com>
2023-05-10 16:12:46 +0800

    refactor：代码重构

--- src/main/kotlin/script/Main.kt
+++ src/main/kotlin/script/Main.kt
@@ -3,18 +3,22 @@ package script
 import java.io.File
 fun main(args: Array<String>) {
     // 指定文件夹路径
-    val folderPath = "path"
+    val folderPath = "D:\\UGit\\icm\\icm-server\\src\\main\\resources\\mapper"
 
 // 指定要删除的字符
-    val charToRemove = "）"
+    val charToRemove = "ODSERPDATA."
 
 // 遍历文件夹下的所有文件
     File(folderPath).walkTopDown().forEach { file ->
         if (file.isFile && file.extension == "xml") {
             // 读取文件内容
             val content = file.readText()
+
+            if(content.contains("to_char")){
+                println(file.name)
+            }
             // 删除指定字符
-            val newContent = content.replace(charToRemove, ")",true)
+            val newContent = content.replace(charToRemove, "",true)
             // 将修改后的内容写回文件
             file.writeText(newContent)
         }
